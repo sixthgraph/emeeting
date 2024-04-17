0d226209f9a516c3adfb9f28e93c0496a13876ae
sixthgraph <sixthgraph@gmail.com>
2024-04-17 22:05:09 +0700

    update mongoose .connect

--- src/configs/dbConfig.ts
+++ src/configs/dbConfig.ts
@@ -1,8 +1,11 @@
 import mongoose from 'mongoose'
 
+// MongoDB connection error. Please make sure MongoDB is running. MongooseError: The `uri` parameter to `openUri()` must be a string, got "undefined". Make sure the first parameter to `mongoose.connect()` or `mongoose.createConnection()` is a string.
+
 export async function dbConnect() {
   try {
-    mongoose.connect(process.env.DATABASE_URL!)
+    // mongoose.connect(process.env.DATABASE_URL!)
+    mongoose.connect('mongodb+srv://sixthgraph:cRIXU5TuO266NPZz@cluster0.dzcgwns.mongodb.net/routeflow')
     const connection = mongoose.connection
 
     connection.on('connected', () => {
