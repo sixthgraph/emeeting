e17c3777fb2eaeef9ddb043d540f4498b1e12876
sixthgraph <sixthgraph@gmail.com>
2024-08-20 14:35:06 +0700

    update user filter by position

--- src/views/apps/user/list/TableFilters.tsx
+++ src/views/apps/user/list/TableFilters.tsx
@@ -19,9 +19,29 @@ const TableFilters = ({ setData, tableData, depData }: { setData: any; tableData
   //const [plan, setPlan] = useState<UsersType['currentPlan']>('')
   const [status, setStatus] = useState<UsersType['status']>('')
   const [department, setDepartment] = useState('')
+  const [position, setPosition] = useState('')
 
-  console.log('tableData === >', tableData)
-  console.log('depData222 === >', depData)
+  const [selectData, setSelectData] = useState(tableData)
+
+  const positionData: any = []
+
+  if (selectData) {
+    for (let elem of selectData) {
+      const posObj: any = elem.dep
+
+      for (let elm of posObj) {
+        const postObj: any = elm
+        const check = positionData.find((item: any) => item.positionid == postObj.positionid)
+        if (check == undefined) {
+          const newData = {
+            positionid: postObj.positionid,
+            positionname: postObj.positionname
+          }
+          positionData.push(newData)
+        }
+      }
+    }
+  }
 
   useEffect(() => {
     const filteredData = tableData?.filter((user: any) => {
@@ -29,60 +49,33 @@ const TableFilters = ({ setData, tableData, depData }: { setData: any; tableData
 
       if (status && user.status !== status) return false
 
-      console.log('department == ')
-      console.log(department)
-
       const foundUser = user.dep.filter((userdep: any) => {
         return userdep.depid == department
       })
 
       if (department !== '' && foundUser == 0) return false
 
+      const foundPos = user.dep.filter((userdep: any) => {
+        return userdep.positionid == position
+      })
+
+      if (position !== '' && foundPos == 0) return false
+
       return true
     })
 
-    //   setData(filteredData)
-    // }, [role, plan, status, tableData, setData])
-
     setData(filteredData)
-  }, [role, status, department, tableData, setData])
+    setSelectData(filteredData)
+  }, [role, status, department, position, tableData, setData])
+
+  useEffect(() => {
+    setPosition('')
+  }, [department])
 
   return (
     <CardContent>
       <Grid container spacing={6}>
-        <Grid item xs={12} sm={4}>
-          <CustomTextField
-            select
-            fullWidth
-            id='select-role'
-            value={role}
-            onChange={e => setRole(e.target.value)}
-            SelectProps={{ displayEmpty: true }}
-          >
-            <MenuItem value=''>Select Role</MenuItem>
-            {/* <MenuItem value='0'>Undefined</MenuItem> */}
-            <MenuItem value='1'>Admin</MenuItem>
-            <MenuItem value='2'>Worker</MenuItem>
-            <MenuItem value='3'>Viewer</MenuItem>
-            <MenuItem value='4'>Super User</MenuItem>
-          </CustomTextField>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <CustomTextField
-            select
-            fullWidth
-            id='select-status'
-            value={status}
-            onChange={e => setStatus(e.target.value)}
-            SelectProps={{ displayEmpty: true }}
-          >
-            <MenuItem value=''>Select Status</MenuItem>
-            <MenuItem value='pending'>Pending</MenuItem>
-            <MenuItem value='active'>Active</MenuItem>
-            <MenuItem value='inactive'>Inactive</MenuItem>
-          </CustomTextField>
-        </Grid>
-        <Grid item xs={12} sm={4}>
+        <Grid item xs={12} sm={3}>
           <CustomTextField
             select
             fullWidth
@@ -99,6 +92,57 @@ const TableFilters = ({ setData, tableData, depData }: { setData: any; tableData
                 </MenuItem>
               )
             })}
+          </CustomTextField>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <CustomTextField
+            select
+            fullWidth
+            id='select-position'
+            value={position}
+            onChange={e => setPosition(e.target.value)}
+            SelectProps={{ displayEmpty: true }}
+          >
+            <MenuItem value=''>Select Position</MenuItem>
+            {positionData.map((pos: any, index: any) => {
+              return (
+                <MenuItem key={index} value={pos.positionid}>
+                  {pos.positionname}
+                </MenuItem>
+              )
+            })}
+          </CustomTextField>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <CustomTextField
+            select
+            fullWidth
+            id='select-role'
+            value={role}
+            onChange={e => setRole(e.target.value)}
+            SelectProps={{ displayEmpty: true }}
+          >
+            <MenuItem value=''>Select Role</MenuItem>
+            {/* <MenuItem value='0'>Undefined</MenuItem> */}
+            <MenuItem value='1'>Admin</MenuItem>
+            <MenuItem value='2'>Worker</MenuItem>
+            <MenuItem value='3'>Viewer</MenuItem>
+            <MenuItem value='4'>Super User</MenuItem>
+          </CustomTextField>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <CustomTextField
+            select
+            fullWidth
+            id='select-status'
+            value={status}
+            onChange={e => setStatus(e.target.value)}
+            SelectProps={{ displayEmpty: true }}
+          >
+            <MenuItem value=''>Select Status</MenuItem>
+            <MenuItem value='pending'>Pending</MenuItem>
+            <MenuItem value='active'>Active</MenuItem>
+            <MenuItem value='inactive'>Inactive</MenuItem>
           </CustomTextField>
         </Grid>
       </Grid>
