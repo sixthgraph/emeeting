04da8f03894d80bca0a2049a1926967749f3dc93
Chulapak Boonyasopon <chulapak@excelink.co.th>
2024-08-22 14:56:30 +0700

    update api edit invite comment

--- src/app/api/comment/edit/route.ts
+++ src/app/api/comment/edit/route.ts
@@ -20,36 +20,36 @@ export async function POST(request: NextRequest) {
 
     const reqBody = await request.json()
 
-    //***body format****/
+    //******/ reqBody format
     // {
     //   "wid": "66ab566acaa7bbd88368defe",
-    //   "email": [
-    //     "chulapak@excelink.co.th",
-    //     "webmaster@excelink.co.th"
-    //     ],
-    //   "invite_by": "supakorn@excelink.co.th"
+    //   "message": "New Reply",
+    //   "file": "",
+    //   "location": "",
+    //   "level": 1,
+    //   "itemno": 2,
+    //   "reply_itemno": 1
     // }
-    //*************/
 
     console.log('reqBody === ')
     console.log(reqBody)
 
-    const res = await axios.post(`${process.env.ROUTE_FLOW_API_URL}/invitecomment`, reqBody, { headers })
+    const res = await axios.post(`${process.env.ROUTE_FLOW_API_URL}/updatecomment`, reqBody, { headers })
 
-    console.log('invite res ----')
+    console.log('updatecomment_response ----')
     console.log(res.data)
-    const delete_res = res.data
+    const update_res = res.data
 
-    if (delete_res.message == 'success') {
+    if (update_res.message == 'success') {
       const response = NextResponse.json({
         message: 'success',
         success: true,
-        data: delete_res.data.detail
+        data: update_res.data.detail
       })
 
       return response
     } else {
-      return NextResponse.json({ error: delete_res.message }, { status: 500 })
+      return NextResponse.json({ error: update_res.message }, { status: 500 })
     }
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 })
