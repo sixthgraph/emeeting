4d88bd56b6a89ecf06c357ac637fbbb06109e42f
Chulapak Boonyasopon <chulapak@excelink.co.th>
2024-08-26 15:37:43 +0700

    update response api getattm

--- src/app/api/attachment/list/route.ts
+++ src/app/api/attachment/list/route.ts
@@ -11,17 +11,29 @@ export async function POST(req: NextRequest) {
   try {
     const headers = { Authorization: `Bearer ${token}`, 'Cache-Control': 'no-cache', Pragma: 'no-cache', Expires: '0' }
 
-    const response = await axios.get(`${process.env.ROUTE_FLOW_API_URL}/getattachment?wid=${wid}&id=${itemno}`, {
+    const res = await axios.get(`${process.env.ROUTE_FLOW_API_URL}/getattachment?wid=${wid}&id=${itemno}`, {
       headers
     })
+
+    const getattm_res = res.data
 
     console.log(wid)
     console.log(itemno)
 
     console.log('----- attm list-------------')
-    console.log(response.data)
+    console.log(res.data)
 
-    return NextResponse.json(response.data)
+    if (getattm_res.message == 'success') {
+      const response = NextResponse.json({
+        message: 'success',
+        success: true,
+        data: getattm_res.data.detail
+      })
+
+      return response
+    } else {
+      return NextResponse.json({ error: getattm_res.message }, { status: 500 })
+    }
   } catch (error: any) {
     return NextResponse.json({ error: error.massage })
   }
