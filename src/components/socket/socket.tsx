1dbdb0b91ea307ae39b065c05e7942519881c8bb
sixthgraph <sixthgraph@gmail.com>
2024-10-31 10:39:38 +0700

    First time connect socket success

--- src/components/socket/socket.tsx
+++ src/components/socket/socket.tsx
@@ -2,7 +2,7 @@
 
 import { io } from 'socket.io-client'
 
-//export const socket = io()
+export const socket = io()
 
 // export const socket = io.connect('https://rd.excelink.co.th:9993', {
 //   secure: false,
@@ -13,13 +13,6 @@ import { io } from 'socket.io-client'
 //   reconnectionAttempts: 5
 // })
 
-const uri = 'http://127.0.0.1:8000'
+// const uri = 'http://127.0.0.1:8000'
 
-export const socket = io(uri, {
-  // secure: false,
-  // rejectUnauthorized: false,
-  // reconnection: true,
-  // reconnectionDelay: 1000,
-  // reconnectionDelayMax: 5000,
-  // reconnectionAttempts: 5
-})
+// export const socket = io(uri)
